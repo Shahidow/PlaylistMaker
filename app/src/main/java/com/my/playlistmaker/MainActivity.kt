a65e48d386d38d5b7f3e9ad105f724b6c5093b4b
Akhmed <shahidow@mail.ru>
2023-09-17 16:54:51 +0300

    Commit 1

--- app/src/main/java/com/my/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/my/playlistmaker/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.my.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+
+        val search = findViewById<Button>(R.id.search)
+        val library = findViewById<Button>(R.id.library)
+        val settings = findViewById<Button>(R.id.settings)
+
+        search.setOnClickListener{
+            val searchIntent = Intent(this, Search::class.java)
+            startActivity(searchIntent)
+        }
+
+        val libraryClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val libraryIntent = Intent(this@MainActivity, Library::class.java)
+                startActivity(libraryIntent)
+            }
+        }
+        library.setOnClickListener(libraryClickListener)
+
+        settings.setOnClickListener{
+            val settingsIntent = Intent(this, Settings::class.java)
+            startActivity(settingsIntent)
+        }
     }
+
 }
---
+++ app/src/main/java/com/my/playlistmaker/Settings.kt
@@ -0,0 +1,22 @@
+package com.my.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Settings : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val back = findViewById<Button>(R.id.backFromSettings)
+
+        back.setOnClickListener{
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+    }
+}
