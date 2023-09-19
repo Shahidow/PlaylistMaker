96a2f148c6ba8dd3a42a5c31fd6e1f67a4e88255
Akhmed <shahidow@mail.ru>
2023-09-19 05:43:04 +0300

    Commit 2

--- app/src/main/java/com/my/playlistmaker/Settings.kt
+++ app/src/main/java/com/my/playlistmaker/Settings.kt
@@ -1,13 +1,11 @@
 package com.my.playlistmaker
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class Settings : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
